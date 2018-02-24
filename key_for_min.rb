63eb86b01c13ed5770bc603be3ce820fe8f4673d
Holden Altaffer <haltaffer@gmail.com>
2018-02-24 10:28:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-result = name_hash.first
+result = name_hash[key]
 name_hash.collect do |key, value|
   if name_hash[key] < result
     result = name_hash[key]
